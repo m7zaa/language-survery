7ccb214180d7bf97108121469d31ac9b8fa57cfc
Micah Zaayer <micahzaayer@gmail.com>
2019-08-16 12:18:16 -0700

    add stying to body, header and survey questions.

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,24 @@ $(document).ready(function(){
     var workRemotely = $("select#workRemotely").val();
     var macPc = $("select#macPc").val();
     var name = $("input#name").val();
+    var javaScript = $("input#javaScript").val();
+    $(".name").text(name);
+
+    if (simplicity==="yes" && windows==="no" && games==="no") {
+      $("#bash, #cSharp, #javaScript").hide();
+      $("#ruby").show();
+    }
+    else if (simplicity==="no" && windows==="yes" && games==="yes") {
+      $("#bash, #ruby, #javaScript").hide();
+      $("#cSharp").show();
+    }
+    else if (simplicity==="yes" && windows==="yes" && games==="no") {
+      $("#bash, #ruby, #cSharp").hide();
+      $("#javaScript").show();
+    }
+    else {
+      $("#ruby, #cSharp, #javaScript").hide();
+      $("#bash").show();
+    }
   });
 });
